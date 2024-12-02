ddbeb4c4b0e4c279fd23ee399f34ff57dd850144
sKzAy <1mkzaexe@gmail.com>
2024-12-02 14:53:46 +0500

    footer added site link

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ const FooterComponent = () => {
   return (
     <div className=' flex1 mt-48 md:mt-44'>
          <div className="thingy bottom-fixed text-white text-xs text-center w-full p-1 ">
-        Made by Kazim Ali for Steam-Nexus at BPC
+        Made by <a className="underline" target='_blank' href="https://kazimali.vercel.app">Kazim Ali</a> for Steam-Nexus at BPC
         </div>
     </div>
   )
